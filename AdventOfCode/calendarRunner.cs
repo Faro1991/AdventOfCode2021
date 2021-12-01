45ea5d972a75d9242e746e4d805c775212fdf136
Fnord <FnordGitHub@outlook.de>
2021-12-01 23:33:47 +0100

    prepwork day 2

--- AdventOfCode/calendarRunner.cs
+++ AdventOfCode/calendarRunner.cs
@@ -14,6 +14,11 @@ namespace AdventOfCode
             day1.input = @"day1/inputDay1.txt";
             day1.dayRun();
 
+            var day2 = new day2();
+            day2.day = 2;
+            day2.input = @"day2/inputDay2.txt";
+            day2.dayRun();
+
             var summary = BenchmarkRunner.Run<day1>();
 
         }
---
+++ AdventOfCode/day2/day2.cs
@@ -0,0 +1,19 @@
+using System;
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode
+{
+    class day2 : dayBase
+    {
+        [Benchmark]
+        public override long partOne()
+        {
+            throw new NotImplementedException();
+        }
+        [Benchmark]
+        public override long partTwo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
