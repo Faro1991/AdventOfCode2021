cbeb234ccb998baeff65a09d1837ec529e1e7f89
FL <Faro1991@users.noreply.github.com>
2021-12-02 13:39:06 +0100

    day 2 part 1: implemented LINQ solution (untested)

--- AdventOfCode/day2/day2.cs
+++ AdventOfCode/day2/day2.cs
@@ -12,6 +12,14 @@ namespace AdventOfCode
         public override long partOne()
         {
             var result = 0;
+            int length = this.items.Where(x => x.Split(" ")[0].ToLower() == "forward").Select(x => Int32.Parse(x.Split(" ")[1])).Sum();
+            int down = this.items.Where(x => x.Split(" ")[0].ToLower() == "down").Select(x => Int32.Parse(x.Split(" ")[1])).Sum();
+            int up = this.items.Where(x => x.Split(" ")[0].ToLower() == "up").Select(x => Int32.Parse(x.Split(" ")[1])).Sum();
+
+            result = length * Math.Abs(down - up);
+
+            // Plain solution, commented out just in case
+            /*
             foreach (string item in this.input)
             {
                 string direction = item.Split(" ")[0].ToLower();
@@ -32,6 +40,7 @@ namespace AdventOfCode
                 }
             }
             result = this._pos[0] * this._pos[1];
+            */
             return result;
         }
         [Benchmark]
