2a960f9464d6ff866e1df1f1777491aa86a0416b
FL <Faro1991@users.noreply.github.com>
2021-12-02 12:17:28 +0100

    implemented day 2 part 1 (untested)

--- AdventOfCode/day2/day2.cs
+++ AdventOfCode/day2/day2.cs
@@ -1,14 +1,38 @@
 using System;
 using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
     class day2 : dayBase
     {
+        private int[,] _pos = new int[1, 1] {0, 0};
+
         [Benchmark]
         public override long partOne()
         {
-            throw new NotImplementedException();
+            var result = 0;
+            foreach (string item in this.input)
+            {
+                string direction = item.Split(" ")[0].ToLower();
+                int amount = Int32.Parse(item.Split(" ")[1]);
+                switch (direction)
+                {
+                    case "forward":
+                        this._pos[0] += amount;
+                        break;
+                    case "up":
+                        this._pos[1] -= amount;
+                        break;
+                    case "down":
+                        this._pos[1] += amount;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            result = this._pos[0] * this._pos[1];
+            return result;
         }
         [Benchmark]
         public override long partTwo()
