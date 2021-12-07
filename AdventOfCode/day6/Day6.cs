894fc5417cf3b03fe61894c249773b2ce199cb2e
Fnord <FnordGitHub@outlook.de>
2021-12-07 20:29:51 +0100

    Switch to Dict state tracking

--- AdventOfCode/day6/Day6.cs
+++ AdventOfCode/day6/Day6.cs
@@ -7,32 +7,63 @@ namespace AdventOfCode
 {
     public class Day6 : DayBase
     {
-        private List<long> _fishNum = new List<long>();
+
+        private Dictionary<long, long> _numberOfFish = new Dictionary<long, long>();
         private List<long> _initialValues = new List<long>();
+
+        private void Initialize()
+        {
+            this._initialValues = this.Items.SelectMany(x => x.Split(",")).ToList().ConvertAll<long>(x => !string.IsNullOrEmpty(x) ? Convert.ToInt64(x) : 0);
+
+            this._numberOfFish.Add(0, 0);
+            this._numberOfFish.Add(1, 0);
+            this._numberOfFish.Add(2, 0);
+            this._numberOfFish.Add(3, 0);
+            this._numberOfFish.Add(4, 0);
+            this._numberOfFish.Add(5, 0);
+            this._numberOfFish.Add(6, 0);
+            this._numberOfFish.Add(7, 0);
+            this._numberOfFish.Add(8, 0);
+            this._numberOfFish.Add(9, 0);
+
+            foreach (long item in _initialValues)
+            {
+                ++this._numberOfFish[item];
+            }
+        }
+
+        private long CalcBirths(int start, int end)
+        {
+            long result = 0;
+
+            for (int i = start; i < end; ++i)
+            {
+                long births = this._numberOfFish[0];
+                this._numberOfFish[7] += births;
+                for (int newFish = 0; newFish < births; ++newFish)
+                {
+                    ++this._numberOfFish[9];
+                }
+                for (int key = 1; key <= 9; ++key)
+                {
+                    long currentNumberOfFish = this._numberOfFish[key];
+                    long newPos = key - 1;
+                    this._numberOfFish[newPos] = currentNumberOfFish;
+                    this._numberOfFish[key] = 0;
+                }
+                List<long> test = this._numberOfFish.Select(x => x.Value).ToList();
+                
+            }
+            result = this._numberOfFish.Values.Select(x => x).Sum();
+            return result;
+        }
         public override long PartOne()
         {
             long result = 0;
-            this._initialValues = this.Items.SelectMany(x => x.Split(",")).ToList().ConvertAll<long>(x => !string.IsNullOrEmpty(x) ? Convert.ToInt64(x) : 0);
-            this._fishNum = this._initialValues.Select(x => x).ToList();
-            for (int i = 0; i < 80; ++i)
-            {
-                long add = this._fishNum.Where(x => x == 0).Count();
-                this._fishNum = this._fishNum.Select(x => --x).ToList();
-                for (int j = 0; j < add; ++j)
-                {
-                    this._fishNum.Add(8);
-                }
-                this._fishNum = this._fishNum.Select(x =>
-                {
-                    if (x < 0)
-                    {
-                        return x + 7;
-                    }
-                    return x;
-                }).ToList();
-            }
 
-            result = this._fishNum.Count();
+            Initialize();
+            
+            result = this.CalcBirths(0, 80);
 
             return result;
         }
@@ -40,6 +71,8 @@ namespace AdventOfCode
         {
             long result = 0;
 
+            result = this.CalcBirths(81, 256);
+        
             return result;
         }
     }
