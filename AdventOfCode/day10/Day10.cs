ea8e03575261a3c6aff63dd20ae8414a2f41796f
Fnord <FnordGitHub@outlook.de>
2021-12-11 21:09:49 +0100

    day 10 done

--- AdventOfCode/day10/Day10.cs
+++ AdventOfCode/day10/Day10.cs
@@ -7,15 +7,123 @@ namespace AdventOfCode
 {
     public class Day10 : DayBase
     {
-        public override long PartOne()
+        private List<string> _cleanedLines = new List<string>();
+        private List<string> _invalidLines = new List<string>();
+        private List<string> _corruptLines = new List<string>();
+        private string[] _validPairs = {"()", "[]", "{}", "<>"};
+        private Dictionary<string, long> _closerValues = new Dictionary<string, long>();
+        private Dictionary<string, long> _completionValues = new Dictionary<string, long>();
+        private void Initialize()
+        {
+            this._closerValues.Add(")", 3);
+            this._closerValues.Add("]", 57);
+            this._closerValues.Add("}", 1197);
+            this._closerValues.Add(">", 25137);
+
+            this._completionValues.Add(")", 1);
+            this._completionValues.Add("]", 2);
+            this._completionValues.Add("}", 3);
+            this._completionValues.Add(">", 4);
+        }
+        private string RemoveValidPairs(string input)
+        {
+            string result = input;
+            foreach (string validPair in this._validPairs)
+            {
+                result = result.Replace(validPair, "");
+            }
+            if (result == input)
+            {
+                return result;
+            }
+            return RemoveValidPairs(result);
+        }
+        private string RemoveOpeners(string input)
+        {
+            string result = input;
+            foreach (string validPair in this._validPairs)
+            {
+                result = result.Replace(validPair[0].ToString(), "");
+            }
+            if (result == input)
+            {
+                return result;
+            }
+            return RemoveOpeners(result);
+
+        }
+        private long CompletingMatchesSum(string input)
         {
             long result = 0;
+            foreach (char c in input)
+            {
+                result *= 5;
+                result += this._completionValues[c.ToString()];
+            }
+            return result;
+        }
+        public override long PartOne()
+        {
+            if (this._closerValues.Count == 0)
+            {
+                this.Initialize();
+            }
+            this._cleanedLines = this.Items.Select(x => RemoveValidPairs(x)).ToList();
+            this._corruptLines = this._cleanedLines.Where(x => 
+            {
+                foreach (char bracket in x)
+                {
+                    if (this._closerValues.Keys.Contains(bracket.ToString()))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }).ToList();
+            this._invalidLines = this._cleanedLines.Where(x => 
+            {
+                foreach (char bracket in x)
+                {
+                    if (this._closerValues.Keys.Contains(bracket.ToString()))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }).ToList();
+
+            long result = 0;
+            List <string> firstClosers = this._corruptLines.Select(x => RemoveOpeners(x)).ToList();
+            result = firstClosers.Select(x => this._closerValues[x[0].ToString()]).Sum();
 
             return result;
         }
         public override long PartTwo()
         {
             long result = 0;
+
+            List<string> completingMatches = this._invalidLines.Select(x => 
+            {
+                string result = "";
+                for (int i = x.Length - 1; i >= 0; --i)
+                {
+                    char c = x[i];
+                    if (c != '(')
+                    {
+                        result += (char) (c + 2);
+                    }
+                    else
+                    {
+                        result += (char) (c + 1);
+                    }
+                }
+                return result;
+            }).ToList();
+
+            List<long> completionResults = completingMatches.Select(x => this.CompletingMatchesSum(x)).ToList();
+            completionResults.Sort();
+
+            result = completionResults[completionResults.Count / 2];
 
             return result;
         }
